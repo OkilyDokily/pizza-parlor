c1d218529e2999e61e28de6e2560bdb4bb84e46c
me <me@me.me.com>
2020-06-04 13:27:13 -0700

    implement addUP pizza spec

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+class Pizza {
+  constructor(){
+  this.veggies = [];
+  this.meats = [];
+  this.size = "";
+  }
+  addUPToppings(){
+   let sizeExtra;
+   if (this.size === "small"){
+     sizeExtra = 0;
+   }
+   else if (this.size === "medium"){
+     sizeExtra = .50;
+   }
+   else {
+     sizeExtra = 1.00
+   }
+   return veggies.length * (1.00 + sizeExtra) + meats.length * (1.50 + sizeExtra)
+  }
+}
+
+$(document).ready(function(){
+
+});
+
+let veggies   
+pineapples: 
+jalapenos: 
+corn: 
+redpeppers: 
+onions: 
+olives:
+
+
+let meats 
+italiansausage
+turkeybacon
+anchovies
+pepperioni
+chicken
+spicychicken
+
+extracheese
